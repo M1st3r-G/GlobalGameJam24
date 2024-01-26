9e00d8dc0f5136f73fe1bf11f8b7879dc698e19d
MisterG <g.haegerbaeumer@gmx.de>
2024-01-26 22:34:15 +0100

    Added BoxSpawn to Attack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour {
     //ComponentReferences
     private InputAction move;
-    private InputAction jump;
     private Rigidbody2D rb;
+    [SerializeField] private GameObject hitBox;
+    [SerializeField] private GameObject hitBoxBig;
     //Params
-    [SerializeField][Range(0,10)]
-    private float speed;
-    [SerializeField] [Range(0, 10)]
-    private float jumpHeight;
+    [SerializeField] [Range(0f,10f)] private float speed;
+    [SerializeField] [Range(0f, 10f)] private float jumpHeight;
     //Temps
-    //Publics
+    //Public
     public enum Directions {
         Up,
         Down,
@@ -34,20 +34,50 @@ public class PlayerController : MonoBehaviour {
         rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
     }
 
-    private void LightAttack(Directions direction) {
-        print("Light Attack");
+    private void LightAttack(Directions direction)
+    {
+        Vector2 pos = transform.position;
+        switch (direction)
+        {
+            case Directions.Up:
+                pos += Vector2.up;
+                break;
+            case Directions.Down:
+                Debug.LogWarning("DownAttackWeak");
+                break;
+            case Directions.Left:
+            case Directions.Right:
+                pos += direction == Directions.Left ? Vector2.left : Vector2.right;
+                break;
+        }
+
+        Instantiate(hitBox, pos, Quaternion.identity);
     }
 
     private void HeavyAttack(Directions direction) {
-        print("Heavy Attack");
+        Vector2 pos = transform.position;
+        switch (direction)
+        {
+            case Directions.Up:
+                pos += Vector2.up;
+                break;
+            case Directions.Down:
+                Debug.LogWarning("DownAttackHeavy");
+                break;
+            case Directions.Left:
+            case Directions.Right:
+                pos += direction == Directions.Left ? Vector2.left : Vector2.right;
+                break;
+        }
+
+        Instantiate(hitBoxBig, pos, Quaternion.identity);
     }
-    
-    
 
     public void OnLightAttackUp(InputAction.CallbackContext ctx) => LightAttack(Directions.Up);
     public void OnLightAttackLeft(InputAction.CallbackContext ctx) => LightAttack(Directions.Left);
     public void OnLightAttackRight(InputAction.CallbackContext ctx) => LightAttack(Directions.Down);
     public void OnLightAttackDown(InputAction.CallbackContext ctx) => LightAttack(Directions.Down);
+    
     public void OnHeavyAttackUp(InputAction.CallbackContext ctx) => HeavyAttack(Directions.Up);
     public void OnHeavyAttackLeft(InputAction.CallbackContext ctx) => HeavyAttack(Directions.Left);
     public void OnHeavyAttackRight(InputAction.CallbackContext ctx) => HeavyAttack(Directions.Right);
